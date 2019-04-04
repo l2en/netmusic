70a0d36ce19e20f5d788e3f5de8741fb87ab6bec
luolin <luolin@luolindeMBP.lan>
2019-04-04 17:24:22 +0800

    歌词滚动

--- pages/music/index.js
+++ pages/music/index.js
@@ -17,16 +17,24 @@ Page({
     duration: 0,
     musicDurationTime: '--:--',
     playedTime: '0:00',
+    lrc: [],
+    playActive: 'music-play-active',
+    currentlrcIndex: null,
+    currentTime: 0,
+    lrcScrollTop: 0
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options = {
+    songid: 571338279
+  }) {
     this.newAduio(options);
     this.songDetails(options.songid);
     this.playEnded();
     this.onPause();
     this.onPlay();
+    this.getLrc(options.songid);
   },
   // 存数据到localstorage
   saveData2Storage(key, val) {
@@ -48,48 +56,47 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.playUpdate()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
     this.backPlay()
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
   newAduio(options) {
@@ -110,15 +117,18 @@ Page({
   },
   // 音乐播放控制
   playControl() {
-    const { playIco } = this.data;
+    const {
+      playIco
+    } = this.data;
     playIco == '../../assets/ico/pause.svg' && innerAudioContext.pause();
     playIco == '../../assets/ico/pause.svg' || innerAudioContext.play();
   },
   // 拖动进度
   timeSliderChanged(e) {
-    console.log('进度', e.detail.value);
-    const { duration} = this.data;
-    let positionNum = duration / 100 * e.detail.value 
+    const {
+      duration
+    } = this.data;
+    let positionNum = duration / 100 * e.detail.value
     this.seek(positionNum)
   },
   // 监听音乐播放进度
@@ -127,8 +137,15 @@ Page({
     innerAudioContext.onTimeUpdate(() => {
       let duration = innerAudioContext.duration,
         currentTime = innerAudioContext.currentTime,
-        sliderCurrentVal = (100 / duration) * currentTime
-      _this.setData({ sliderCurrentVal, playedTime: time.timeFormat(currentTime) })
+        sliderCurrentVal = (100 / duration) * currentTime;
+      let currentlrcIndex= _this.handleCurrentIndex(currentTime);
+      console.log('>>', currentlrcIndex)
+      _this.setData({
+        sliderCurrentVal,
+        currentlrcIndex,
+        currentTime,
+        playedTime: time.timeFormat(currentTime)
+      })
     })
   },
   // 跳转至指定位置
@@ -152,7 +169,7 @@ Page({
   canPlay() {
     const _this = this
     innerAudioContext.onCanplay(() => {
-      setTimeout(function () {
+      setTimeout(function() {
         let duration = innerAudioContext.duration
         _this.setData({
           musicDurationTime: time.timeFormat(duration),
@@ -180,18 +197,17 @@ Page({
     })
   },
   // 获取歌曲详情
-  songDetails(id = 1348044876) {
+  songDetails(id = 571338279) {
     const _this = this
     wx.request({
       url: `http://wyyyy.xyz:3000/song/detail?ids=${id}`,
       success: res => {
-        console.log('获取成功', res);
         if (res.data.code != 200) return;
         _this.setData({
           songDetails: res.data.songs[0]
         })
       },
-      fail: err => { }
+      fail: err => {}
     })
   },
   // 后台播放
@@ -214,6 +230,63 @@ Page({
   moreList() {
     this.setData({
       showPlayList: false
+    })
+  },
+  // 歌词获取
+  getLrc(id = 571338279) {
+    const _this = this;
+    // 歌词格式化成数组
+    function parseLyric(text) {
+      var lines = text.split('\n'), //将文本按行分隔，存入数组
+        pattern = /\[\d*:\d*((\.|\:)\d*)*\]/g, //正则表达式
+        result = []; //保存最终结果的数组
+      lines.forEach(function(v, index, arr) {
+        var time = v.match(pattern), //返回与正则匹配的字符串的数组，正则中有/g，为全部
+          value = v.replace(pattern, ''); //提取歌词
+        if (time) {
+          var t = time[0].slice(1, -1).split(':'); //去掉时间里的中括号并分割
+          if (t.length === 3) { //[00:00:00]
+            result.push([parseInt(t[0], 10) * 60 + parseInt(t[1]) + parseFloat("0." + t[2]), value]); //最终数组
+          } else {
+            result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
+          }
+        }
+      });
+      //将元素按时间大小排序，貌似没有必要
+      result.sort(function(a, b) {
+        return a[0] - b[0];
+      });
+      return result;
+    }
+    wx.request({
+      url: `http://wyyyy.xyz:3000/lyric?id=${id}`,
+      success: res => {
+        console.log('获取lrc', res);
+        if (res.data.nolyric) {
+          _this.setData({
+            lrc: [['nolrc', '暂无歌词']]
+          })
+        } else {
+          const lrc = parseLyric(res.data.lrc.lyric)
+          _this.setData({
+            lrc
+          })
+        }
+      }
+    })
+  },
+  // 滑动歌词
+  scrollChange(e) {
+    console.log('滑动', e.detail);
+  },
+  // 确定当前歌词index
+  handleCurrentIndex(currentTime){
+    const _this = this
+    const { lrc } = this.data
+    console.log(this.data.duration, lrc[lrc.length - 1][0])
+    let lrcScrollTop = (this.data.duration / lrc[lrc.length - 1][0]) * currentTime*21
+    this.setData({
+      lrcScrollTop
     })
   }
 })
